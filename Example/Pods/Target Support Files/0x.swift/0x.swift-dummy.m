299f694ad8185e3624d25f7ed896b514fa9578e1
Vadim Koleoshkin <vadim@koleoshkin.com>
2019-03-28 14:36:30 -0700

    remove pods from spec

--- Example/Pods/Target Support Files/0x.swift/0x.swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_0x_swift : NSObject
-@end
-@implementation PodsDummy_0x_swift
-@end
--- Example/Pods/Target Support Files/Pods-0x.swift_Tests/Pods-0x.swift_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_0x_swift_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_0x_swift_Tests
-@end
