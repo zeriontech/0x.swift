041f5c0c0a98066e0c83cc390cda2bd980d064d9
Igor <igor.shmakov@yahoo.com>
2019-04-09 14:26:13 -0400

    Implemented EIP712 typed data encoding

---
+++ 0x.swift/EIP712/EIP712Domain.swift
@@ -0,0 +1,11 @@
+//
+// This source file is part of the 0x.swift open source project
+// Copyright 2019 The 0x.swift Authors
+// Licensed under Apache License v2.0
+//
+// EIP712Domain.swift
+//
+// Created by Igor Shmakov on 09/04/2019
+//
+
+import Foundation
