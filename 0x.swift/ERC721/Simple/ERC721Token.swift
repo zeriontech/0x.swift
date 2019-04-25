aea5609cf13e6325501de1dcd5376d7def961cc9
Vadim Koleoshkin <vadim@koleoshkin.com>
2019-04-24 22:02:39 -0700

    add encoder

---
+++ 0x.swift/ERC721/Simple/ERC721Token.swift
@@ -0,0 +1,18 @@
+//
+// This source file is part of the 0x.swift open source project
+// Copyright 2019 The 0x.swift Authors
+// Licensed under Apache License v2.0
+//
+// ERC721Token.swift
+//
+// Created by Vadim Koleoshkin on 24/04/2019
+//
+
+import Foundation
+import Web3Swift
+
+class ERC721Token: ERC721 {
+    
+    
+    
+}
